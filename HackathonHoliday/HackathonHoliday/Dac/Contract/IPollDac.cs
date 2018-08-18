1119c51bd5e6301a8fc001c6041313d8d9c02553
Kritsada Dechawantana <kritsada@perfenterprise.com>
2018-08-18 12:23:32 +0700

    add poll dac

---
+++ HackathonHoliday/HackathonHoliday/Dac/Contract/IPollDac.cs
@@ -0,0 +1,15 @@
+﻿using HackathonHoliday.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackathonHoliday.Dac.Contract
+{
+    public interface IPollDac
+    {
+        Task CreatePoll(Poll model);
+        Task<IEnumerable<Poll>> GetPolls();
+    }
+}
---
+++ HackathonHoliday/HackathonHoliday/Dac/PollDac.cs
@@ -0,0 +1,34 @@
+﻿using HackathonHoliday.Dac.Contract;
+using HackathonHoliday.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HackathonHoliday.Dac
+{
+    public class PollDac : IPollDac
+    {
+        private IMongoCollection<Poll> collection;
+
+        public PollDac()
+        {
+            var client = new MongoClient("mongodb://unipopcorn:MTu3D1EBGdfcAoFpC4GpzUHzqELbRdSfuLZb01Exsdunf4wKv8y6dT7MbANP4y6w8UqlUtvFAuMAD22YbfxF3g==@unipopcorn.documents.azure.com:10255/?ssl=true&replicaSet=globaldb");
+            var database = client.GetDatabase("unipopcorn");
+            collection = database.GetCollection<Poll>("poll");
+        }
+
+        public async Task CreatePoll(Poll model)
+        {
+            await collection.InsertOneAsync(model);
+        }
+
+        public async Task<IEnumerable<Poll>> GetPolls()
+        {
+            var model = await collection.Find(x => true).ToListAsync();
+            return model;
+        }
+    }
+}
