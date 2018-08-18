ffd602fe8542e5999f9172750ea1888d32dd07f6
Kritsada Dechawantana <kritsada@perfenterprise.com>
2018-08-18 15:15:28 +0700

    implement vote

--- HackathonHoliday/HackathonHoliday/Controllers/HomeController.cs
+++ HackathonHoliday/HackathonHoliday/Controllers/HomeController.cs
@@ -88,10 +88,15 @@ namespace HackathonHoliday.Controllers
         {
             var poll = await pollDac.GetPoll(pollid);
             var choices = await choiceDac.GetChoiceByPollId(poll.Id);
-            var ch = choices.Select(it => new ChoiceInformation
-            {
-                PollRefId = poll.Id,
-                Name = it.Title
+            var ch = choices.Select(it => {
+                var rate = it.Votes != null && it.Votes.Any() ? (int)Math.Floor(it.Votes.Sum(v => v.Rating) / it.Votes.Count()) : 0;
+                return new ChoiceInformation
+                {
+                    PollRefId = poll.Id,
+                    Name = it.Title,
+                    Rate = rate,
+                    Id = it.Id
+                };
             });
 
             var pollInfo = new PollInformation
@@ -119,8 +124,22 @@ namespace HackathonHoliday.Controllers
         }
 
         [HttpGet]
-        public ActionResult ChoiceDetail(string choiceid)
+        public async Task<ActionResult> ChoiceDetail(string choiceid)
         {
+            var choice = await choiceDac.GetChoice(choiceid);
+            var poll = await pollDac.GetPoll(choice.PollId);
+            var rate = choice.Votes != null && choice.Votes.Any() ? (int)Math.Floor(choice.Votes.Sum(it => it.Rating) / choice.Votes.Count()) : 0 ;
+            var model = new DisplayChoiceInformation
+            {
+                Id = choice.Id,
+                Name = choice.Title,
+                PollRefId = poll.Id,
+                PollCreateDate = poll.CreateAt,
+                PollName = poll.Topic,
+                PollOwnerName = poll.Owner,
+                Rate = rate
+            };
+
             return View(new DisplayChoiceInformation());
         }
 
@@ -128,20 +147,23 @@ namespace HackathonHoliday.Controllers
         public async Task<ActionResult> ChoiceDetail(DisplayChoiceInformation model)
         {
             var choice = await choiceDac.GetChoice(model.Id);
+            if (choice.Votes == null) choice.Votes = new List<Vote>();
+
             var voted = choice.Votes.FirstOrDefault(it => it.Owner == User.Identity.Name);
             var votes = choice.Votes.ToList();
             if (voted != null)
             {
                 var voteIndex = votes.IndexOf(voted);
-                votes[voteIndex].Rating = 3;
+                votes[voteIndex].Rating = model.Rate;
                 votes[voteIndex].CreateAt = DateTime.UtcNow;
+               
             }
             else
             {
                 votes.Add(new Vote
                 {
                     Owner = User.Identity.Name,
-                    Rating = 3,
+                    Rating = model.Rate,
                     CreateAt = DateTime.UtcNow
                 });
             }
